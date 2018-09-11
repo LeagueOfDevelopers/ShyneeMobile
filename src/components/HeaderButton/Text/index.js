1d06365de3355ca2223d965ac5c2c0a644647477
Mafark <nikyyaa@gmail.com>
2018-09-11 10:57:47 +0300

    Refactoring

--- src/components/HeaderButton/Text/index.js
+++ src/components/HeaderButton/Text/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import Button from '../components/Button';
+import Button from '../../Button';
 
 import styles from './styles';
 
--- src/components/HeaderButton/Text/styles.js
+++ src/components/HeaderButton/Text/styles.js
@@ -1,6 +1,6 @@
 import {StyleSheet } from 'react-native';
 
-import {fonts, colors} from '../../constants/styles';
+import {fonts, colors} from '../../../constants/styles';
 
 export default StyleSheet.create({
   button: {
