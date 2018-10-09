3915bdfb58e19f92a61074350096aa320625fd7f
Mafark <nikyyaa@gmail.com>
2018-10-09 17:00:50 +0300

    add text-area

--- src/components/Form/TextField/index.js
+++ src/components/Form/TextField/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import { View, TextInput } from 'react-native';
 
@@ -6,52 +6,59 @@ import Text from '../../Text';
 
 import styles from './styles';
 
-const TextField = (
-  {
-    title,
-    placeholder,
-    multiline = false,
-    activePlaceholder,
-    style,
-    containerStyle,
-    input,
-    meta,
-    ...inputProps
-  }
-) => {
-  const error = meta.touched && !meta.valid;
-  const expanded = input.value || meta.active;
-  return (
-    <View style={[styles.container, containerStyle]}>
-      <View style={[styles.inputContainer, meta.active
-        ? styles.activeContainer
-        : (error ? styles.errorContainer : styles.inactiveContainer)]}>
-        {expanded
-          ? <Text style={[styles.title, error ? styles.errorTitle : styles.validTitle]}>
-            {title}
-          </Text>
-          : null}
-        <TextInput
-          {...inputProps}
-          {...meta}
-          onChangeText={input.onChange}
-          onBlur={input.onBlur}
-          onFocus={input.onFocus}
-          value={input.value}
-          placeholder={expanded ? activePlaceholder : placeholder}
-          multiline={multiline}
-          style={[
-            styles.input,
-            expanded ? styles.activeInput : styles.inactiveInput,
-            input.value && meta.valid && !meta.active ? styles.validInput : styles.invalidInput,
-            style
-          ]}
-        />
+class TextField extends PureComponent {
+  render() {
+    const {
+      title,
+      placeholder,
+      multiline = false,
+      activePlaceholder,
+      style,
+      containerStyle,
+      input,
+      meta,
+      ...inputProps
+    } = this.props;
+    const error = meta.touched && !meta.valid;
+    const expanded = input.value || meta.active;
+
+    return (
+      <View style={containerStyle}>
+        <View style={[
+          styles.inputContainer,
+        ]}>
+          {
+            expanded &&
+            (<Text style={[styles.title, error ? styles.errorTitle : styles.validTitle]}>
+              {title}
+            </Text>)
+          }
+          <TextInput
+            {...inputProps}
+            {...meta}
+            onChangeText={input.onChange}
+            onBlur={input.onBlur}
+            onFocus={input.onFocus}
+            value={input.value}
+            placeholder={expanded ? activePlaceholder : placeholder}
+            multiline={multiline}
+            style={[
+              styles.input,
+              input.value && meta.valid && !meta.active ? styles.validInput : styles.invalidInput,
+              style,
+            ]}
+          />
+        </View>
+        <View style={[
+          styles.line,
+          meta.active && styles.activeLine,
+          error && styles.errorLine,
+        ]} />
+        {error ? <Text style={styles.error}>{meta.error}</Text> : null}
       </View>
-      {error ? <Text style={styles.error}>{meta.error}</Text> : null}
-    </View>
-  );
-};
+    );
+  }
+}
 
 TextField.propTypes = {
   title: PropTypes.string,
